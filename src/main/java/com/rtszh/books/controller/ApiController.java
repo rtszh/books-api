8da808303689e3cbb3797a06052e48892265af83
rtszh <arthur.sazhnev@yandex.ru>
2022-09-26 11:18:55 +0300

    add 4 endpoints

--- src/main/java/com/rtszh/books/controller/ApiController.java
+++ src/main/java/com/rtszh/books/controller/ApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/books")
 @OpenAPIDefinition(
         info = @Info(
-                title = "books service api",
+                title = "Books service api",
                 description = "Test",
                 version = "0.1.1"
         )
@@ -27,18 +27,12 @@ public class ApiController {
     }
 
     @GetMapping
-//    @Operation(summary = "Get all books ordered by title descending",
-//            description = "endpoint возвращающий список все книг, которые содержатся в таблице book, отсортированный в обратном алфавитном порядке значения колонки book.title"
-//    )
     public ResponseEntity<?> getAllBooksOrderedByTitleDesc() {
         var bookList = bookService.getAllBooksOrderedByTitleDesc();
         return ResponseEntity.ok(bookList);
     }
 
     @GetMapping("/by-author")
-//    @Operation(summary = "Get all books by authors",
-//            description = "endpoint возвращающий список всех книг, сгруппированных по автору книги(book.author)"
-//    )
     public ResponseEntity<?> getAllBooksGroupByAuthors() {
         var bookMap = bookService.getAllBooksGroupByAuthors();
 
@@ -46,11 +40,6 @@ public class ApiController {
     }
 
     @GetMapping("/by-symbol")
-//    @Operation(summary = "Get all books by symbols",
-//            description = "endpoint принимающий в качестве параметра символ и возвращающий список из 10 авторов, " +
-//                    "в названии книг которых этот символ встречается наибольшее количество раз " +
-//                    "вместе с количеством вхождений этого символа во все названия книг автора"
-//    )
     public ResponseEntity<?> getAuthorsBySymbols(
             @RequestParam String symbol
     ) {
@@ -61,9 +50,6 @@ public class ApiController {
     }
 
     @PostMapping
-//    @Operation(summary = "Add new book to database",
-//            description = "endpoint добавления новой книги в таблицу book"
-//    )
     public ResponseEntity<?> addBook(@RequestBody BookDto bookDto) {
 
         bookService.addBook(bookDto);
